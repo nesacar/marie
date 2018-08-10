e6c70ff0c31b95d4e4cab8c4923c557c00db97fb
VacaSan <kotaracvladan@gmail.com>
2018-08-10 09:26:31 +0200

    Subscription image not loading fix.

--- resources/views/themes/marie-clarie/partials/subscription.blade.php
+++ resources/views/themes/marie-clarie/partials/subscription.blade.php
@@ -1,7 +1,10 @@
 <div class="subscription">
   <span class="subscription_label">novo izdanje</span>
   <a href="{{ $settings->magazine_link }}">
-    <div class="image image--3-4 subscription_image" data-src="{{ $settings->magazine_image? url($settings->magazine_image): '' }}" data-alt="{{ $settings->magazine_title }}"></div>
+    <div class="image image--3-4 js-lazy-image subscription_image"
+      data-src="{{ $settings->magazine_image? url($settings->magazine_image): '' }}"
+      data-alt="{{ $settings->magazine_title }}"
+    ></div>
   </a>
   <a class="btn btn--primary btn--block" href="{{ $settings->magazine_link }}">pretplati se</a>
 </div>
