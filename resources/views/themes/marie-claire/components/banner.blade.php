7142fc5652fefe7004db6698828b936fde98f63e
pancevac <sinisa.buncic7@gmail.com>
2018-09-03 10:53:51 +0200

    Fix banner position_id.

--- resources/views/themes/marie-claire/components/banner.blade.php
+++ resources/views/themes/marie-claire/components/banner.blade.php
@@ -1,5 +1,5 @@
 @if(!empty($position))
     <div class="{{ $position->class }}">
-        <ins data-revive-zoneid="{{ $position->id }}" data-revive-id="84b8c658a018e14743ef895ae9380fa0"></ins>
+        <ins data-revive-zoneid="{{ $position->position_id }}" data-revive-id="84b8c658a018e14743ef895ae9380fa0"></ins>
     </div>
 @endif
